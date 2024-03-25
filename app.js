224859908b8ac78c378e151c1db077824e8eb93e
CodedBytes <dev.joaovictor22@gmail.com>
2024-03-25 16:59:26 -0300

    feat: design for index and status done.

--- app.js
+++ app.js
@@ -20,9 +20,23 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/public/css')));
+app.use(express.static(path.join(__dirname, '/public/imgs')));
+app.use(express.static(path.join(__dirname, '/public/imgs/icons')));
 
 // Pagina
 app.get('/', (req, res) => { res.render(path.join(__dirname + '/public/views/index.ejs')); });
+
+// Pagina registro de etiqueta
+app.get('/register', (req, res) => { res.render(path.join(__dirname + '/public/views/register.ejs')); });
+
+// Pagina status de etiqueta
+app.get('/status', (req, res) => { res.render(path.join(__dirname + '/public/views/status.ejs')); });
+
+// Pagina Banco de dados de etiqueta
+app.get('/database', (req, res) => { res.render(path.join(__dirname + '/public/views/database.ejs')); });
+
+// Pagina gerenciamento de usuarios
+app.get('/users', (req, res) => { res.render(path.join(__dirname + '/public/views/users.ejs')); });
 
 // Servindo o webapp
 httpsServer.listen(443, () => {
